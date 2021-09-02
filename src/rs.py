ce19765723a40d95aee09e938be03b098b980a51
kamuri <myk.gata14@gmail.com>
2021-09-02 19:21:26 -0300

    choose to run as rustc or cargo

--- src/rs.py
+++ src/rs.py
@@ -3,6 +3,16 @@ import os
 from src.parser import popen
 import pathlib
 
+
+def checkRunAsRustc(file):
+    with open(file, "r", encoding="utf-8") as f:
+       lines = [x.strip('\n') for x in f.readlines()]
+    if len(lines) > 1:
+        if lines[0] == "// rustc":
+            return True
+    return False
+
+
 def rust(path: str, filename: str, *args) -> int:
     file_basename = os.path.basename(filename)
     file_basename = file_basename.split(".")
@@ -13,10 +23,13 @@ def rust(path: str, filename: str, *args) -> int:
     # compile
     if popen("rustc", "-o", targ_path, *args, filename) == 0:
         # run
+        print("runnnig")
         return popen(targ_path)
 
 
 def rsCargo(path: str, filename: str, *args) -> bool:
+    if checkRunAsRustc(filename):
+        return False
     parent_path = str(pathlib.Path(path).parent.absolute())
     files_in_parent = os.listdir(parent_path)
     if "Cargo.toml" in files_in_parent:
